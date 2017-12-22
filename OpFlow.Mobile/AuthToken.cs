e55b8c6016a81be32179f36e2543f339023f4389
Jeff <jeff@thesandjay.com>
2017-12-22 15:59:00 -0600

    add security layer to web service requests

---
+++ OpFlow.Mobile/AuthToken.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace OpFlow.Mobile
+{
+    public class AuthToken
+    {
+        [JsonProperty("access_token")]
+        public string AccessToken { get; set; }
+        [JsonProperty("token_type")]
+        public string TokenType { get; set; }
+        [JsonProperty("expires_in")]
+        public int ExpiresIn { get; set; }
+        [JsonProperty("userName")]
+        public string UserName { get; set; }
+        [JsonProperty(".issued")]
+        public string Issued { get; set; }
+        [JsonProperty(".expires")]
+        public string Expires { get; set; }
+
+        public DateTime IssuedDate => DateTime.Parse(Issued);
+
+        public DateTime ExpiresDate => DateTime.Parse(Expires);
+    }
+}
--- OpFlow.Mobile/WebUtility.cs
+++ OpFlow.Mobile/WebUtility.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
 using OpFlow.Data;
@@ -13,17 +14,21 @@ namespace OpFlow.Mobile
     public abstract class WebUtility
     {
         private static HttpClient _client;
-        private static string _rootUrl = "https://opflowservice.azurewebsites.net/api";
+        private static string _rootUrl = "https://opflowservice.azurewebsites.net";
+
+        private static AuthToken _authToken;
 
         static WebUtility()
         {
             _client = new HttpClient();
+
+            _client.BaseAddress = new Uri(_rootUrl);
             _client.MaxResponseContentBufferSize = 256000;
         }
 
         public static async Task<List<Schedule>> GetSchedules()
         {
-            var response = await WebRequest("schedule");
+            var response = await WebRequest("api/schedule");
 
             var result = JsonConvert.DeserializeObject<List<Schedule>>(response);
 
@@ -32,7 +37,7 @@ namespace OpFlow.Mobile
 
         public static async Task<Schedule> GetSchedule(int scheduleId)
         {
-            var response = await WebRequest(string.Format("schedule/{0}", scheduleId));
+            var response = await WebRequest(string.Format("api/schedule/{0}", scheduleId));
 
             var result = JsonConvert.DeserializeObject<Schedule>(response);
 
@@ -45,6 +50,12 @@ namespace OpFlow.Mobile
 
             var uri = Path.Combine(_rootUrl, command);
 
+            if (_authToken == null || _authToken.ExpiresDate < DateTime.Now)
+            {
+                await LoginUser();
+            }
+
+            _client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", _authToken?.AccessToken);
             using (var response = await _client.GetAsync(uri))
             {
                 if (response.StatusCode != HttpStatusCode.OK)
@@ -54,6 +65,37 @@ namespace OpFlow.Mobile
             }
 
             return result;
+        }
+
+        private static async Task LoginUser()
+        {
+            AuthToken authToken = null;
+
+            var formData = new List<KeyValuePair<string, string>>();
+            formData.Add(new KeyValuePair<string, string>("grant_type", "password"));
+            formData.Add(new KeyValuePair<string, string>("username", "OpFlow@OpFlow.com"));
+            formData.Add(new KeyValuePair<string, string>("password", "OpFlow1!"));
+
+            try
+            {
+                var request = new HttpRequestMessage(HttpMethod.Post, "/Token");
+                request.Content = new FormUrlEncodedContent(formData);
+
+                using (var response = await _client.SendAsync(request))
+                {
+                    if (response.StatusCode != HttpStatusCode.OK)
+                        Console.Out.WriteLine("Error fetching data. Server returned status code: {0}", response.StatusCode);
+
+                    var responseString = await response.Content.ReadAsStringAsync();
+
+                    _authToken = JsonConvert.DeserializeObject<AuthToken>(responseString);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
         }
     }
 }
