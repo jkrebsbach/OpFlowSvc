e26810fc7e2a280a49675c442e51cca4b25a5ae1
Jeff <jeff@thesandjay.com>
2017-12-26 10:23:58 -0600

    add test project

---
+++ OpFlow.Mobile.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("OpFlow.Mobile.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("OpFlow.Mobile.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("3630e12b-d8f2-460d-be38-a6360e235beb")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ OpFlow.Mobile.Test/WebUtilityTest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace OpFlow.Mobile.Test
+{
+    [TestClass]
+    public class WebUtilityTest
+    {
+        [TestMethod]
+        public async Task TestSendMessage()
+        {
+            await WebUtility.GetSchedules();
+        }
+    }
+}
