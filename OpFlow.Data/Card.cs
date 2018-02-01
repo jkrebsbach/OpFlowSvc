d91dc3b559bec743a88e86f29befbd2d0fd4d887
Jeff <jeff@thesandjay.com>
2018-02-01 17:48:01 -0600

    adding additional fields to card entity

--- OpFlow.Data/Card.cs
+++ OpFlow.Data/Card.cs
@@ -9,7 +9,12 @@ namespace OpFlow.Data
         public int CardID { get; set; }
         public int ProcedureID { get; set; }
         public int SpecialtyID { get; set; }
+        public int FlowID { get; set; }
+        public int RoomConfigID { get; set; }
+        public int TimesUsed { get; set; }
         public string ProcedureDescription { get; set; }
+        public string FlowDescription { get; set; }
         public string CardDescription { get; set; }
+        public decimal AvgMinutes { get; set; }
     }
 }
