3dda8cd9212eb40b21c45b98f5432f91bf4753e5
Jeff <jeff@thesandjay.com>
2019-07-17 10:09:26 -0500

    new controller for disposable item rationalization

---
+++ OpFlow.Service/Controllers/ItemRationalizationController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Http;
+using OpFlow.Data;
+using OpFlow.Service.DataAccess;
+using Swashbuckle.Swagger.Annotations;
+
+namespace OpFlow.Service.Controllers
+{
+    [Authorize]
+    [RoutePrefix("api/itemRationalization")]
+    public class ItemRationalizationController : ApiController
+    {
+
+        // GET api/surgery?surgeryId=5&caseId=1&providerId=1&bundleFlag=Y
+        [SwaggerOperation("GetItemRationalizations")]
+        [SwaggerResponse(HttpStatusCode.OK, Type = typeof(List<ItemMaster>))]
+        [Route("item")]
+        public async Task<HttpResponseMessage> GetItemRationalizations(int specialtyId, decimal? minCost, decimal? maxCost)
+        {
+            var user = await CacheUtil.GetUserSecurity();
+            var sqlHelper = new SqlHelper(user.CaseDatabaseName);
+
+            var items = await sqlHelper.GetItems(null, null, true, user.ProviderID, user.LocationID);
+
+            return Request.CreateResponse(HttpStatusCode.OK, items);
+        }
+    }
+}
