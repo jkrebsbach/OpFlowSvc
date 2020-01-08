a21f5740f5b80cd989bc61809f1edcd127b6e791
Jeff <jeff@thesandjay.com>
2020-01-08 09:11:26 -0600

    fix ratios for supply savings report

--- OpFlow.Data/Analytics/AnalyticsSummary.cs
+++ OpFlow.Data/Analytics/AnalyticsSummary.cs
@@ -187,8 +187,8 @@ namespace OpFlow.Data
         public decimal WasteCost { get; set; }
         public decimal OverallocatedUnits { get; set; }
         public decimal OverallocatedCost { get; set; }
-        public decimal WasteReduction => (WasteCost / TotalCost);
-        public decimal OverallocatedReduction => (OverallocatedCost / TotalCost);
+        public decimal WasteReduction => (WasteCost / TotalCost) * 100;
+        public decimal OverallocatedReduction => (OverallocatedCost / TotalCost) * 100;
     }
 
     public class CountSummaryReportPost
