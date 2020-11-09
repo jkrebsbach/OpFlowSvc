58f59021bbd6cb08d7a37e80739ef79a5bbd22dd
Jeff <jeff@thesandjay.com>
2020-11-09 16:31:03 -0600

    add product nbr to item counts page

--- OpFlow.Data/ItemMaster.cs
+++ OpFlow.Data/ItemMaster.cs
@@ -13,6 +13,7 @@ namespace OpFlow.Data
         public string UnitOfMeasure { get; set; }
         public string ItemType { get; set; }
         public string Category { get; set; }
+        public string ProductNbr { get; set; }
         public string ItemDescription { get; set; }
         public decimal UnitCost { get; set; }
         public decimal BillableUnitCost { get; set; }
