242f006c89fe006c843e8768bf0d990e7b88bbad
Jeff <jeff@thesandjay.com>
2023-07-01 16:29:14 -0500

    Workflow to parse CMC cards into CSV file

--- OpFlow.Service.Test/PDFTester.cs
+++ OpFlow.Service.Test/PDFTester.cs
@@ -7,6 +7,8 @@ using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Xml.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NPOI.XWPF.UserModel;
+using OpFlow.Data.Administration;
 using OpFlow.Service.DataAccess;
 using WebSupergoo.ABCpdf11;
 using WebSupergoo.ABCpdf11.Objects;
@@ -26,7 +28,7 @@ namespace OpFlow.Service.Test
         [TestMethod]
         public async Task TestReadPDF()
         {
-            var path = @"D:\ColdStorage\Documents\OpFlow\Imports\20230506\";
+            var path = @"D:\ColdStorage\Documents\OpFlow\Imports\20230701\";
 
             var directories = Directory.GetDirectories(path);
             foreach (var directory in directories)
@@ -267,6 +269,391 @@ namespace OpFlow.Service.Test
             return fileImport;
         }
 
+
+        [TestMethod]
+        public async Task TestReadCMCPDF()
+        {
+            var path = @"D:\ColdStorage\Documents\OpFlow\Imports\20230701\";
+
+            var directories = Directory.GetDirectories(path);
+            foreach (var directory in directories)
+            {
+                var files = Directory.GetFiles(directory, "*.pdf");
+
+                var outputData = "Surgeon,CardName,Manufacturer,Description,ProductNbr,Open,Prn\r\n";
+
+                foreach (var file in files)
+                {
+                    var cardImport = ProcessCMCFile(file);
+
+                    foreach (var cardItem in cardImport)
+                    {
+                        // double quote issue, quick hack
+                        if (cardItem.ProductNbr == string.Empty)
+                            cardItem.ProductNbr = " ";
+
+                        //if (cardItem.Quantity == "")
+                        //    continue;
+
+                        outputData += $"\"{cardItem.SurgeonName}\",\"{cardItem.CardName}\",\"{cardItem.Manufacturer} \",\"{cardItem.Description.Replace("\"", "\"\"")}\",\"{cardItem.ProductNbr}\",{cardItem.Open},{cardItem.Prn}\r\n";
+                    }
+                }
+
+                var filename = $"{Path.GetFileName(directory)}_CardData.csv";
+                var outputFile = Path.Combine(path, filename);
+                File.WriteAllText(outputFile, outputData);
+            }
+        }
+        private List<CardImport> ProcessCMCFile(string filePath)
+        {
+            var pdfPageText = new List<string>();
+
+            using (var doc = new Doc())
+            {
+                doc.Read(filePath);
+
+                int theCount = doc.PageCount;
+                for (int i = 1; i <= theCount; i++)
+                {
+                    doc.PageNumber = i;
+                    pdfPageText.Add(doc.GetText(Page.TextType.Svg, false));
+                }
+            }
+
+            var cardImport = new List<CardImport>();
+
+            var cardNameDetail = string.Empty;
+            var surgeonName = string.Empty;
+
+            var supplies = false;
+            var instruments = false;
+
+            foreach (var pageText in pdfPageText)
+            {
+                var document = XDocument.Parse(pageText);
+
+                foreach (var graphic in document.Root.Elements())
+                {
+                    if (graphic.Name.LocalName == "text")
+                    {
+
+                        var fontSize = graphic.Attribute("font-size").Value;
+                        var fontFamily = graphic.Attribute("font-family").Value;
+
+                        var data = ParseToken(graphic);
+
+                        if (data.Trim() == "Surgeon:")
+                        {
+                            surgeonName = string.Empty;
+                            var surgeonX = graphic.Attribute("x").Value;
+                            var surgeonY = graphic.Attribute("y").Value;
+
+                            foreach (var testCell in document.Root.Elements())
+                            {
+                                var testRowX = testCell.Attribute("x")?.Value;
+                                var testRowY = testCell.Attribute("y")?.Value;
+                                if (surgeonY == testRowY && surgeonX != testRowX)
+                                    surgeonName += testCell.Value;
+                            }
+                        }
+                        if (data.Trim() == "Procedures:")
+                        {
+                            cardNameDetail = string.Empty;
+                            var procedureCell = (graphic.NextNode as XElement).Attribute("y").Value;
+
+                            foreach (var testCell in document.Root.Elements())
+                            {
+                                var testRow = testCell.Attribute("y")?.Value;
+                                if (procedureCell == testRow)
+                                    cardNameDetail += testCell.Value;
+                            }
+                        }
+
+                        if (data == "Supplies")
+                            supplies = true;
+
+                        if (data == "Drugs")
+                            supplies = false;
+
+                        if (data == "Bin Location" && supplies)
+                            cardImport.AddRange(ParseSupplyTable(graphic, surgeonName, cardNameDetail));
+
+                        if (data == "Instruments")
+                            instruments = true;
+
+                        if (data == "Bin Location" && instruments)
+                            cardImport.AddRange(ParseInstrumentTable(graphic, surgeonName, cardNameDetail));
+
+                        if (data == "Implant Trays")
+                            instruments = false;
+
+                    }
+                }
+
+            }
+
+            if (cardNameDetail == string.Empty) throw new Exception("Unable to parse card" + filePath);
+
+            return cardImport;
+        }
+
+        private List<CardImport> ParseSupplyTable(XElement headerElement, string surgeonName, string cardNameDetail)
+        {
+            var cardImport = new List<CardImport>();
+
+            var tableHeader = decimal.Parse(headerElement.Attribute("y").Value);
+            
+            bool newRow = false;
+            decimal curPos = 0.0M;
+
+            decimal? descriptionPosition = null;
+            decimal? openPosition = null;
+            decimal? prnPosition = null;
+
+            var tableName = "Supplies";
+            var productNbr = string.Empty;
+            var description = string.Empty;
+            var openValue = string.Empty;
+            var prnValue = string.Empty;
+
+            foreach (var graphic in headerElement.Document.Root.Elements())
+            {
+                if (graphic.Name.LocalName == "text")
+                {
+
+
+                    var fontSize = graphic.Attribute("font-size").Value;
+                    var fontFamily = graphic.Attribute("font-family").Value;
+
+                    var xLoc = decimal.Parse(graphic.Attribute("x").Value);
+                    var yLoc = decimal.Parse(graphic.Attribute("y").Value);
+
+                    if (tableHeader > yLoc)
+                        continue;
+
+                    var data = ParseToken(graphic);
+
+                    if (tableHeader == yLoc)
+                    {
+                        if (data == "Item Name")
+                        {
+                            descriptionPosition = xLoc;
+                        }
+                        if (data == "Open")
+                        {
+                            openPosition = xLoc;
+                        }
+                        if (data == "PRN")
+                        {
+                            prnPosition = xLoc;
+                        }
+
+                        continue;
+                    }
+
+                    if (data == "Drugs")
+                        break;
+
+                    if (data == "Mfg. #: ")
+                    {
+                        productNbr = (graphic.NextNode as XElement).Value;
+                        newRow = true;
+                    }
+
+                    if (xLoc == descriptionPosition)
+                    {
+                        curPos = yLoc;
+
+                        if (description != string.Empty) 
+                            description = $"{description.Trim()} ";
+                    }
+
+                    if (xLoc >= descriptionPosition && xLoc < openPosition && yLoc == curPos)
+                    {
+                        description += data;
+                    }
+
+                    if (xLoc == openPosition && yLoc > tableHeader)
+                    {
+                        openValue = data;
+                    }
+
+                    if (xLoc == prnPosition && yLoc > tableHeader)
+                    {
+                        prnValue = data;
+                    }
+
+                    //if (xLoc > productNumPosition && yLoc > tableHeader && description != string.Empty) 
+                    if (newRow)
+                    {
+                        if (description == string.Empty)
+                        {
+                            newRow = false;
+                            continue;
+                        }
+
+                        if (data == "Drugs") return cardImport;
+
+                        cardImport.Add(new CardImport()
+                        {
+                            SurgeonName = surgeonName,
+                            CardName = cardNameDetail,
+                            Manufacturer = tableName.Trim(),
+                            Description = description.Trim(),
+                            ProductNbr = productNbr.Trim(),
+                            Open = openValue,
+                            Prn = prnValue
+                        });
+
+                        description = string.Empty;
+                        productNbr = string.Empty;
+                        openValue = string.Empty;
+                        prnValue = string.Empty;
+
+                        newRow = false;
+                    }
+                }
+            }
+
+            // add remaining data
+            if (description != string.Empty)
+            {
+                cardImport.Add(new CardImport()
+                {
+                    SurgeonName = surgeonName,
+                    CardName = cardNameDetail,
+                    Manufacturer = tableName.Trim(),
+                    Description = description.Trim(),
+                    ProductNbr = productNbr.Trim(),
+                    Open = openValue,
+                    Prn = prnValue
+                });
+            }
+
+            return cardImport;
+        }
+
+        private List<CardImport> ParseInstrumentTable(XElement headerElement, string surgeonName, string cardNameDetail)
+        {
+            var cardImport = new List<CardImport>();
+            var tableHeader = decimal.Parse(headerElement.Attribute("y").Value);
+
+            bool newRow = false;
+            decimal curPos = 0.0M;
+
+            decimal? binLocationPosition = null;
+            decimal? descriptionPosition = null;
+            decimal? openPosition = null;
+
+            var tableName = "Instruments";
+            var description = string.Empty;
+            var openValue = string.Empty;
+
+            foreach (var graphic in headerElement.Document.Root.Elements())
+            {
+                if (graphic.Name.LocalName == "text")
+                {
+                    var fontSize = graphic.Attribute("font-size").Value;
+                    var fontFamily = graphic.Attribute("font-family").Value;
+
+                    var xLoc = decimal.Parse(graphic.Attribute("x").Value);
+                    var yLoc = decimal.Parse(graphic.Attribute("y").Value);
+
+                    if (tableHeader > yLoc)
+                        continue;
+
+                    var data = ParseToken(graphic);
+
+                    if (tableHeader == yLoc)
+                    {
+                        if (data == "Bin Location")
+                        {
+                            binLocationPosition = xLoc;
+                        }
+                        if (data == "Item Name")
+                        {
+                            descriptionPosition = xLoc;
+                        }
+                        if (data == "Open")
+                        {
+                            openPosition = xLoc;
+                        }
+
+                        continue;
+                    }
+
+                    if (data == "Implant Trays")
+                        break;
+
+
+                    if (xLoc == binLocationPosition)
+                    {
+                        newRow = curPos > 0;
+                        curPos = yLoc;
+                    }
+
+                    if (xLoc == descriptionPosition)
+                    {
+                        curPos = yLoc;
+
+                        if (description != string.Empty)
+                            description = $"{description.Trim()} ";
+                    }
+
+                    if (xLoc >= descriptionPosition && xLoc < openPosition && yLoc == curPos)
+                    {
+                        description += data;
+                    }
+
+                    if (xLoc == openPosition && yLoc > tableHeader)
+                    {
+                        openValue = data;
+                    }
+
+                    //if (xLoc > productNumPosition && yLoc > tableHeader && description != string.Empty) 
+                    if (newRow)
+                    {
+                        if (description == string.Empty)
+                        {
+                            newRow = false;
+                            continue;
+                        }
+
+                        if (data == "Drugs") return cardImport;
+
+                        cardImport.Add(new CardImport()
+                        {
+                            SurgeonName = surgeonName,
+                            CardName = cardNameDetail,
+                            Manufacturer = tableName.Trim(),
+                            Description = description.Trim(),
+                            Open = openValue,
+                        });
+
+                        description = string.Empty;
+                        openValue = string.Empty;
+
+                        newRow = false;
+                    }
+                }
+            }
+
+            // add remaining data
+            if (description != string.Empty)
+            {
+                cardImport.Add(new CardImport()
+                {
+                    SurgeonName = surgeonName,
+                    CardName = cardNameDetail,
+                    Manufacturer = tableName.Trim(),
+                    Description = description.Trim(),
+                    Open = openValue,
+                });
+            }
+
+            return cardImport;
+        }
+
         private bool CheckRange(decimal xLoc, decimal? target)
         {
             if (target == null) return false;
@@ -295,10 +682,13 @@ namespace OpFlow.Service.Test
         private class CardImport
         {
             public string CardName { get; set; }
+            public string SurgeonName { get; set; }
             public string Manufacturer { get; set; }
             public string Description { get; set; }
             public string ProductNbr { get; set; }
             public string Quantity { get; set; }
+            public string Open { get; set; }
+            public string Prn { get; set; }
         }
 
         [TestMethod]
